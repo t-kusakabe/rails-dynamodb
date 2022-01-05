67d29a6a58a0aae31ba9b8a56beac4c8c85a2e6a
t-kusakabe <ttmac0704@gmail.com>
2022-01-05 10:51:06 +0900

    Rename controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,27 @@
+class TasksController < ApplicationController
+  def index
+    task = Task.all
+
+    render json: :ok
+  end
+
+  def create
+    binding.pry
+    task = Task.create(name: tasks_params[:id])
+
+    render json: task
+  end
+
+  def destroy
+    task = Task.find_by_id()
+    task.delete
+
+    render json: :ok
+  end
+
+  private
+
+  def tasks_params
+    params.permit(:id)
+  end
+end
--- app/controllers/tests_controller.rb
+++
@@ -1,5 +0,0 @@
-class TestsController < ApplicationController
-  def index
-    render json: :ok
-  end
-end
